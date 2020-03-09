4bff731a2dad058d442afad76159345b624f4b66
D:\\Git Repo <craig.burger@outlook.com>
2020-03-09 19:27:10 +0200

    First upload of online portfolio

---
+++ js/site.js
@@ -0,0 +1,13 @@
+function click1() {
+    location.href = 'http://www.petsco.co.za';
+}
+
+function click2() {
+    location.href = 'https://github.com/CraigRichardBurger/Capstone-Project-1';
+}
+
+function click3() {
+    location.href = 'http://www.gunlicence.co.za';
+}
+
+//The above functions add onclick links to our buttons. They allow the user to click a button and access the respective project.
